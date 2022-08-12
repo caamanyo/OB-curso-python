5cc3f93ca2067b97f1c73b0fce4fb38079c7b340
Miquel Caamaño <kamanyo@gmail.com>
2022-08-12 12:09:33 +0200

    4.2

--- 4.Control_flujo/ejercicio2.py
+++ 4.Control_flujo/ejercicio2.py
@@ -1,1 +1,10 @@
-print("hola")
+numero_inicial = int(input("Escoge un número inicial:"))
+numero_final = int(input("Escoge un número final:"))
+lista_impares = []
+
+for num in range(numero_inicial, numero_final + 1):
+    if num % 2 != 0:
+        lista_impares.append(num)
+
+print(f"Listado de números impares entre {numero_inicial} y {numero_final}:")
+print(lista_impares)
