331af80d4d35b70cfc836d420c9a9929079d41f0
Miquel Caamaño Salom <kamanyo@gmailcom>
2022-08-14 19:53:56 +0200

    Ejercicios tema 8

--- 8.Entrada_salida/ejercicio1/ejercicio1.py
+++ 8.Entrada_salida/ejercicio1/ejercicio1.py
@@ -1,5 +1,18 @@
 path = '8.Entrada_salida/ejercicio1/hola.txt'
+
 file = open(path, 'w')
-buffer = "Esto es un archivo de texto."
-data = file.write(buffer)
+buffer = 'Esto es un archivo de texto.\n'
+file.write(buffer)
+buffer = 'Esta es la siguiente linea del archivo.\n'
+file.write(buffer)
+buffer = 'Última línea.\n'
+file.write(buffer)
+
+file.close()
+
+file = open(path, 'r')
+
+for line in file.readlines():
+    print(line)
+
 file.close()
